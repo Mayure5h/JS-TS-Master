a8421aa10dc3c64df6a497cedb63e01a71c5c421
Mayure5h <42106970+Mayure5h@users.noreply.github.com>
2019-11-24 11:21:32 +0530

    Add files via upload

--- Promise.js
+++ Promise.js
@@ -14,3 +14,34 @@ p.then((message) => {
 }).catch((message) => {
     console.log("This is in the catch " + message)
 })
+
+//Another example similar to callback 
+
+let userLeft = false;
+let userWatchingCatMeme = false; 
+
+function watchTutorialCallback(){
+    return new Promise((resolve, reject) => {
+        if(userLeft){
+            reject({
+                name: 'user left',
+                message: ':('            
+            })
+        } else if(userWatchingCatMeme){
+            reject({
+                name: 'user watching cat meme',
+                message: ':('
+            })
+        } else{
+            resolve('Everything is fine!')
+        }
+    })
+}
+
+watchTutorialCallback().then((message) => {
+    console.log('Success: ' + message);
+}).then((message) => {                      //We can call multiple then
+    console.log('Success: ' + message); 
+}).catch((error) => {
+    console.log(error.name + ' ' + error.message);
+})
