14da7bcf2da7d005026bf74e67e0dc7cf2f48743
Mayure5h <42106970+Mayure5h@users.noreply.github.com>
2020-06-13 20:12:10 +0530

    Update Normal JS functions.js

--- Normal JS functions.js
+++ Normal JS functions.js
@@ -3,3 +3,18 @@
 {Object.keys(yourObject).map(function(key) {
     return <div>Key: {key}, Value: {yourObject[key]}</div>;
 })}
+
+***********************************************************************
+//Find array value based on key value 
+    
+var myArray = [
+ {id:1, name:"bob"},
+ {id:2, name:"dan"},
+ {id:3, name:"barb"},
+]
+
+// grab the Array item which matchs the id "2"
+var item = myArray.find(item => item.id === 2);
+
+// print
+console.log(item.name);
